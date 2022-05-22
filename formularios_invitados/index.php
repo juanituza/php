c4044d3d780297af7bff87edf98b6322d1a59339
juanituza <juanituza85@gmail.com>
2022-05-22 20:44:37 -0300

    cambios

--- formularios_invitados/index.php
+++ formularios_invitados/index.php
@@ -17,24 +17,22 @@ if ($_POST) {
 
     if (isset($_POST["btnProcesar"])) {
 
-        //Si el dni ingresado se encuentra en la lista se mostrara un mensaje de bienvenida
-        if (in_array($dni, $aInvitados)) {
+        //Si el dni ingresado se encuentra en la lista y el codigo es correcto
+        // se mostrara un mensaje de bienvenida y su codigo de acceso
+        if (in_array($dni, $aInvitados) && $codigo == 'verde') {
             $bienvenida = "$dni se encuentra en la lista de invitados";
+            $codAcceso = 'Su codigo de acceso es' . " " . rand(1000, 9999);
+        } else // si el dni ingresado se encuentra en la lista y el codigo no es correcto
+        // se mostrara un mensaje de bienvenida y Ud. no es VIP
+        if (in_array($dni, $aInvitados) && $codigo != 'verde') {
+            $bienvenida = "$dni se encuentra en la lista de invitados";
+            $errorVip = 'Ud. no es VIP';
         } else {
             //si no un mensaje de "no se encuentra en la lista de invitados"
             $error = "$dni no se encuentra en la lista";
         }
     }
-    if (isset($_POST["btnVip"])) {
-
-        //Si el código es verde entonces se mostrará "su codigo de acceso es (ramdom)"
-        if ($codigo == 'verde') {
-            $codAcceso = 'Su codigo de acceso es' . " " . rand(1000, 9999);
-        } //si no un mensaje "Ud. no es VIP" 
-        else {
-            $errorVip = 'Ud. no es VIP';
-        }
-    }
+    
 }
 
 ?>
@@ -60,21 +58,23 @@ if ($_POST) {
                 <h1>Lista de invitados</h1>
             </div>
             <?php if (isset($bienvenida)) : ?>
-                <div class=" col-4 alert alert-success shadow" role="alert">
+                <div class=" col-4 mx-4 alert alert-success shadow" role="alert">
                     <?php echo $bienvenida; ?>
                 </div>
-            <?php endif; if (isset($error)) : ?>
-                <div class="col-4 alert alert-danger shadow" role="alert">
+            <?php endif;
+            if (isset($error)) : ?>
+                <div class="col-4 mx-4 alert alert-danger shadow" role="alert">
                     <?php echo $error; ?>
                 </div>
             <?php endif; ?>
             <?php if (isset($codAcceso)) : ?>
-                <div class="col-4 alert alert-success shadow" role="alert">
+                <div class="col-4 px-4 alert alert-success shadow" role="alert">
                     <?php echo $codAcceso; ?>
                 </div>
-            <?php endif; if (isset($errorVip)) : ?>
-                <div class="col-4 alert alert-danger shadow" role="alert">
-                    <?php echo $errorVip;?>
+            <?php endif;
+            if (isset($errorVip)) : ?>
+                <div class="col-4 mx-4 alert alert-danger shadow" role="alert">
+                    <?php echo $errorVip; ?>
                 </div>
             <?php endif; ?>
         </div>
@@ -85,16 +85,12 @@ if ($_POST) {
                         <label for="txtDni" class="py-2">Ingrese DNI:</label>
                         <input type="text" name="txtDni" id="txtDni" class="form-control shadow">
                     </div>
-                    <div class="my-3">
-                        <button type="submit" name="btnProcesar" id="btnProcesar" class="btn btn-outline-primary shadow py-1">Verificar invitado</button>
-                    </div>
-
                     <div class="mb-3 mt-5">
                         <label for="txtCodigo" class="py-2">Ingrese el código secreto para el pase VIP:</label>
                         <input type="text" name="txtCodigo" id="txtCodigo" class="form-control shadow">
                     </div>
                     <div class="my-3">
-                        <button type="submit" name="btnVip" id="btnVip" class="btn btn-outline-primary shadow py-1">Verificar código</button>
+                        <button type="submit" name="btnProcesar" id="btnProcesar" class="btn btn-outline-primary shadow py-1">Verificar invitado</button>
                     </div>
                 </form>
             </div>
