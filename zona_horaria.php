9d7ba5633f1e6b7571d1d32c19a5c09734a1184d
juanituza <juanituza85@gmail.com>
2022-06-03 10:58:40 -0300

    cambios

---
+++ zona_horaria.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+//url : https://www.php.net/manual/es/timezones.php
+
+?>
